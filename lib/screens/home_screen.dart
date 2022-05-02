7940a94d02d888c4a8d8905642eb4ab0f54b2c91
EFTEKHER <eftekherali2000@gmail.com>
2022-05-03 03:31:36 +0600

    Day2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../services/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  FirebaseFirestore firestore=FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,6 +27,42 @@ class _HomeScreenState extends State<HomeScreen> {
               label: Text("Sign out"),
               style: TextButton.styleFrom(primary: Colors.white))
         ],
+      ),
+
+      body: Container(width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+ElevatedButton(onPressed: ()async
+{
+  //create random id
+  // CollectionReference users=firestore.collection('users');
+  // await users.add({'name':'Efte'});
+  CollectionReference users=firestore.collection('users');
+  await users.doc('flutter 123').set({
+    'name':'Tonmoy'
+  });
+
+}, child: Text("Add Data To Firestore")),
+
+ElevatedButton(onPressed: ()async{
+//for read all value
+   CollectionReference users=firestore.collection('users');
+  // QuerySnapshot allResults = await users.get();
+  // allResults.docs.forEach((DocumentSnapshot results) { 
+  //   print(results.data());
+ // });
+
+ //for read particular element
+ DocumentSnapshot result=await users.doc('flutter 123').get();
+ print(result.data());
+
+}, child: Text('Read Data from Firestore'))
+        ],
+        
+      ),
+      
       ),
     ));
   }
